5a1f63a880f0cb57b1e39fff84bce7faac55aa7d
11090815 <1378406814@mail.nwpu.edu.cn>
2024-06-07 20:39:30 +0800

    gossip/protoext

---
+++ gossip/protoext/signedgossipmessage.go
@@ -0,0 +1,177 @@
+package protoext
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/11090815/mayy/errors"
+	"github.com/11090815/mayy/protobuf/pgossip"
+	"google.golang.org/protobuf/proto"
+)
+
+type SignFuncType func(msg []byte) ([]byte, error)
+
+type VerifyFuncType func(peerIdentity, signature, message []byte) error
+
+type SignedGossipMessage struct {
+	*pgossip.Envelope
+	*pgossip.GossipMessage
+}
+
+func (sgm *SignedGossipMessage) Sign(signFunc SignFuncType) (*pgossip.Envelope, error) {
+	// 不要修改私密数据
+	var secretEnvelope *pgossip.SecretEnvelope
+	if sgm.Envelope != nil {
+		secretEnvelope = sgm.Envelope.SecretEnvelope
+	}
+	sgm.Envelope = nil
+	payload, err := proto.Marshal(sgm.GossipMessage)
+	if err != nil {
+		return nil, err
+	}
+	signature, err := signFunc(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	envelope := &pgossip.Envelope{
+		Payload:        payload,
+		Signature:      signature,
+		SecretEnvelope: secretEnvelope,
+	}
+	sgm.Envelope = envelope
+	return envelope, nil
+}
+
+func (sgm *SignedGossipMessage) Verify(peerIdentity []byte, verifyFunc VerifyFuncType) error {
+	if sgm.Envelope == nil {
+		return errors.NewError("missing envelope")
+	}
+	if err := verifyFunc(peerIdentity, sgm.Envelope.Signature, sgm.Envelope.Payload); err != nil {
+		return err
+	}
+	if sgm.Envelope.SecretEnvelope != nil {
+		return verifyFunc(peerIdentity, sgm.Envelope.SecretEnvelope.Signature, sgm.Envelope.SecretEnvelope.Payload)
+	}
+	return nil
+}
+
+func (sgm *SignedGossipMessage) IsSigned() bool {
+	return sgm.Envelope != nil && sgm.Envelope.Payload != nil && sgm.Envelope.Signature != nil
+}
+
+func (sgm *SignedGossipMessage) String() string {
+	envelope := "Nil-Envelope"
+	if sgm.Envelope != nil {
+		var secretEnvelope string
+		if sgm.SecretEnvelope != nil {
+			secretEnvelopePayloadLen := len(sgm.SecretEnvelope.Payload)
+			secretEnvelopeSignatureLen := len(sgm.SecretEnvelope.Signature)
+			secretEnvelope = fmt.Sprintf("{SecretEnvelope | Payload: %dbytes; Signature %dbytes}", secretEnvelopePayloadLen, secretEnvelopeSignatureLen)
+		}
+		if secretEnvelope != "" {
+			envelope = fmt.Sprintf("{{Envelope | Payload: %dbytes; Signature: %dbytes} %s}", len(sgm.Envelope.Payload), len(sgm.Envelope.Signature), secretEnvelope)
+		} else {
+			envelope = fmt.Sprintf("{Envelope | Payload: %dbytes; Signature: %dbytes}", len(sgm.Envelope.Payload), len(sgm.Envelope.Signature))
+		}
+	}
+
+	gossipMessage := "Nil-GossipMessage"
+	var isSimple bool = false
+	if sgm.GossipMessage != nil {
+		if sgm.GetRemoteStateRes() != nil {
+			gossipMessage = RemoteStateResponseToString(sgm.GetRemoteStateRes())
+		} else if sgm.GetDataMsg() != nil && sgm.GetDataMsg().Payload != nil {
+			gossipMessage = PayloadToString(sgm.GetDataMsg().Payload)
+		} else if sgm.GetDataUpdate() != nil {
+			gossipMessage = DataUpdateToString(sgm.GetDataUpdate())
+		} else if sgm.GetMemRes() != nil {
+			gossipMessage = MembershipResponseToString(sgm.GetMemRes())
+		} else if sgm.GetStateInfoSnapshot() != nil {
+			gossipMessage = StateInfoSnapshotToString(sgm.GetStateInfoSnapshot())
+		} else if sgm.GetRemotePvtDataRes() != nil {
+			gossipMessage = RemotePvtDataResponseToString(sgm.GetRemotePvtDataRes())
+		} else if sgm.GetAliveMsg() != nil {
+			gossipMessage = AliveMessageToString(sgm.GetAliveMsg())
+		} else if sgm.GetMemReq() != nil {
+			gossipMessage = MembershipRequestToString(sgm.GetMemReq())
+		} else if sgm.GetStateInfoPullReq() != nil {
+			gossipMessage = StateInfoPullRequestToString(sgm.GetStateInfoPullReq())
+		} else if sgm.GetStateInfo() != nil {
+			gossipMessage = StateInfoToString(sgm.GetStateInfo())
+		} else if sgm.GetDataDig() != nil {
+			gossipMessage = DataDigestToString(sgm.GetDataDig())
+		} else if sgm.GetDataReq() != nil {
+			gossipMessage = DataRequestToString(sgm.GetDataReq())
+		} else if sgm.GetLeadershipMsg() != nil {
+			gossipMessage = LeadershipMessageToString(sgm.GetLeadershipMsg())
+		} else {
+			gossipMessage = sgm.GossipMessage.String()
+			isSimple = true
+		}
+		if !isSimple {
+			description := fmt.Sprintf("{Description | Channel: %s, Nonce: %d, Tag: %s}", hex.EncodeToString(sgm.Channel), sgm.Nonce, sgm.Tag.String())
+			gossipMessage = fmt.Sprintf("{%s %s}", description, gossipMessage)
+		}
+	}
+
+	return fmt.Sprintf("{SignedGossipMessage | GossipMessage: %s; Envelope: %s}", gossipMessage, envelope)
+}
+
+/* ------------------------------------------------------------------------------------------ */
+
+func SignSecret(envelope *pgossip.Envelope, signFunc SignFuncType, secret *pgossip.Secret) error {
+	payload, err := proto.Marshal(secret)
+	if err != nil {
+		return err
+	}
+	signature, err := signFunc(payload)
+	if err != nil {
+		return err
+	}
+	envelope.SecretEnvelope = &pgossip.SecretEnvelope{
+		Payload:   payload,
+		Signature: signature,
+	}
+	return nil
+}
+
+func NoopSign(gm *pgossip.GossipMessage) (*SignedGossipMessage, error) {
+	sgm := &SignedGossipMessage{
+		GossipMessage: gm,
+	}
+	payload, err := proto.Marshal(sgm.GossipMessage)
+	if err != nil {
+		return nil, err
+	}
+	sgm.Envelope = &pgossip.Envelope{
+		Payload: payload,
+	}
+
+	return sgm, nil
+}
+
+func EnvelopeToSignedGossipMessage(envelope *pgossip.Envelope) (*SignedGossipMessage, error) {
+	if envelope == nil {
+		return nil, errors.NewError("nil envelope")
+	}
+	gm := &pgossip.GossipMessage{}
+	if err := proto.Unmarshal(envelope.Payload, gm); err != nil {
+		return nil, err
+	}
+	return &SignedGossipMessage{
+		GossipMessage: gm,
+		Envelope:      envelope,
+	}, nil
+}
+
+func InternalEndpoint(se *pgossip.SecretEnvelope) string {
+	if se == nil {
+		return ""
+	}
+	secret := &pgossip.Secret{}
+	if err := proto.Unmarshal(se.Payload, secret); err != nil {
+		return ""
+	}
+	return secret.GetInternalEndpoint()
+}
---
+++ gossip/protoext/stringers.go
@@ -0,0 +1,93 @@
+package protoext
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/11090815/mayy/protobuf/pgossip"
+)
+
+func MemberToString(m *pgossip.Member) string {
+	return fmt.Sprintf("{Member | Endpoint: %s; PKI-ID: %s}", m.Endpoint, hex.EncodeToString(m.PkiId))
+}
+
+func MembershipResponseToString(mr *pgossip.MembershipResponse) string {
+	return fmt.Sprintf("{MembershipResponse | Alive-Number: %d; Dead-Number: %d}", len(mr.Alive), len(mr.Dead))
+}
+
+func AliveMessageToString(am *pgossip.AliveMessage) string {
+	var identity string
+	if len(am.Identity) == 0 {
+		identity = "nil-identity"
+	} else if len(am.Identity) < 32 {
+		identity = hex.EncodeToString(am.Identity)
+	} else {
+		identity = hex.EncodeToString(am.Identity[:8]) + "..." + hex.EncodeToString(am.Identity[len(am.Identity)-8:])
+	}
+
+	return fmt.Sprintf("{AliveMessage | Endpoint: %s; PKI-ID: %s; PeerTime-IncNum: %d; PeerTime-SeqNum: %d; Identity: %s}",
+		am.Membership.Endpoint, hex.EncodeToString(am.Membership.PkiId), am.Timestamp.IncNum, am.Timestamp.SeqNum, identity)
+}
+
+func PayloadToString(p *pgossip.Payload) string {
+	return fmt.Sprintf("{Payload | SeqNum: %d; Data: %dbytes; PrivateData: %ditems}", p.SeqNum, len(p.Data), len(p.PrivateData))
+}
+
+func DataUpdateToString(du *pgossip.DataUpdate) string {
+	return fmt.Sprintf("{DataUpdate | Nonce: %d; Data: %ditems; MsgType: %s}", du.Nonce, len(du.Data), pgossip.PullMsgType_name[int32(du.MsgType)])
+}
+
+func StateInfoSnapshotToString(sis *pgossip.StateInfoSnapshot) string {
+	return fmt.Sprintf("{StateInfoSnapshot | Elements: %ditems}", len(sis.Elements))
+}
+
+// TODO 弄清楚 MembershipRequest.SelfInformation 到底是什么
+func MembershipRequestToString(mr *pgossip.MembershipRequest) string {
+	return fmt.Sprintf("{MembershipRequest | Known: %d}", len(mr.Known))
+}
+
+func StateInfoPullRequestToString(sipr *pgossip.StateInfoPullRequest) string {
+	return fmt.Sprintf("{StateInfoPullRequest | Channel-MAC: %s}", hex.EncodeToString(sipr.Channel_MAC))
+}
+
+func StateInfoToString(si *pgossip.StateInfo) string {
+	return fmt.Sprintf("{StateInfo | PeerTime-IncNum: %d; PeerTime-SeqNum: %d; PKI-ID: %s; Channel-MAC: %s; LedgerHight: %d; LeftChannel: %v}",
+		si.Timestamp.IncNum, si.Timestamp.SeqNum, hex.EncodeToString(si.PkiId), hex.EncodeToString(si.Channel_MAC), si.Properties.LedgerHeight, si.Properties.LeftChannel)
+}
+
+func DataDigestToString(dd *pgossip.DataDigest) string {
+	digests := "["
+	for i, digest := range dd.Digests {
+		digests = digests + fmt.Sprintf("%d:%s", i, hex.EncodeToString(digest))
+		if i < len(dd.Digests)-1 {
+			digests = digests + ", "
+		}
+	}
+	digests = digests + "]"
+	return fmt.Sprintf("{DataDigest | Nonce: %d; MsgType: %s; Digests: %s}", dd.Nonce, dd.MsgType.String(), digests)
+}
+
+func DataRequestToString(dr *pgossip.DataRequest) string {
+	digests := "["
+	for i, digest := range dr.Digests {
+		digests = digests + fmt.Sprintf("%d:%s", i, hex.EncodeToString(digest))
+		if i < len(dr.Digests)-1 {
+			digests = digests + ", "
+		}
+	}
+	digests = digests + "]"
+	return fmt.Sprintf("{DataDigest | Nonce: %d; MsgType: %s; Digests: %s}", dr.Nonce, dr.MsgType.String(), digests)
+}
+
+func LeadershipMessageToString(lm *pgossip.LeadershipMessage) string {
+	return fmt.Sprintf("{LeadershipMessage | PKI-ID: %s; IsDeclaration: %v; PeerTime-IncNum: %d; PeerTime-SeqNum: %d}",
+		hex.EncodeToString(lm.PkiId), lm.IsDeclaration, lm.Timestamp.IncNum, lm.Timestamp.SeqNum)
+}
+
+func RemotePvtDataResponseToString(rpdr *pgossip.RemotePvtDataResponse) string {
+	return fmt.Sprintf("{RemotePvtDataResponse | Elements: %ditmes}", len(rpdr.Elements))
+}
+
+func RemoteStateResponseToString(rsr *pgossip.RemoteStateResponse) string {
+	return fmt.Sprintf("{RemoteStateResponse | Payloads: %ditems}", len(rsr.Payloads))
+}
